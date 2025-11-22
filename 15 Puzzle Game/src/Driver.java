0b94de17d13e298228752554b2ff69ba0bd52cbd
Mikaz Navodya <mikaznavodya@gmail.com>
2025-11-22 09:28:58 +0530

    Update Driver.java

--- 15 Puzzle Game/src/Driver.java
+++ 15 Puzzle Game/src/Driver.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Ahsan Riaz
- */
 public class Driver {
     public static void main(String[] args) {
         System.out.println("testing");
